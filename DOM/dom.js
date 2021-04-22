656ebf1575042f0660186c8ec3b33f0bfed8945e
alejandroBarua <2baruaalejandro95@gmail.com>
2021-04-21 23:03:00 -0300

    Termine el DOM y cambie el diseño de Lenguaje

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,29 +1,15 @@
-console.log(document);
-console.log(document.head);
-console.log(document.body);
-console.log(document.charset);
-console.log(document.title);
-console.log(document.links); // lista de links no es un array
-console.log(document.images); 
-console.log(document.forms);
-console.log(document.styleSheets);
-console.log(document.scripts);
-
 /* setTimeout(() => {
     
-    console.log(document.getSelection().toString()); // mostrara lo seleccionado
+    console.log(document.getSelection().toString()); // mostrara lo seleccionado con el mouse en cada momento
     
 }, 3000); */
 
 //document.write("<h2> hola mundo desde el DOM</h2>"); // no es buena practica utilizarlo
 
-console.clear();
 
-// clase 62: elementos del dom
-
+// seleccionar elementos del dom
 
 console.log(document.getElementById("menu")); // me devuelve el codigo html
-
 
 console.log(document.querySelector("li")); // me devulvera el html del figure que este primero
 console.log(document.querySelectorAll("li")); // me devuelve una nodelista con todas las etiquetas li del documento
@@ -37,76 +23,50 @@ console.log(document.querySelector("#menu")); // no reemplaza a getElementById
 console.log(document.querySelectorAll("#menu li")); // solo quiero las li que estan dentro del menu
 
 document.querySelectorAll("a").forEach(el => console.log(el)); // muestro cada etiqueta en codigo html
+// la nodeLista lo puedo recorrer y acceder a una posicion como lo hago en un array
 
 console.clear();
 
-// clase 63: atributos y data-atributos ( para crear nuestros propios atributos)
 
-// estas dos dan lo mismo pero la mejor manera es hacerlo con getAtributte
-
-console.log(document.documentElement.lang);
-console.log(document.documentElement.getAttribute("lang"));
-
-// estas dos dan cosas diferentes 
-
-console.log(document.querySelector(".link-dom").href);
-console.log(document.querySelector(".link-dom").getAttribute("href"));
-
-document.documentElement.lang = "es"; // cambio valor de la propiedad
-console.log(document.documentElement.getAttribute("lang"));
-
-document.documentElement.setAttribute("lang", "es-MX");
-console.log(document.documentElement.getAttribute("lang"));
-
-
+// atributos 
 
 const $linkDOM = document.querySelector(".link-dom"); // $ hace referencia a que es un elemento del DOM
 
+$linkDOM.setAttribute("href", "https://developer.mozilla.org/es/docs/Glossary/DOM");
 $linkDOM.setAttribute("target", "_blank");
 $linkDOM.setAttribute("rel", "noopener"); // por seguridad de la nueva ventana que estamos abriendo
-$linkDOM.setAttribute("href", "https://www.google.com/");
 
+console.log($linkDOM.getAttribute("href"))
 console.log($linkDOM.hasAttribute("rel")); // me devuelve true si encuentra el atributo en el elemento
 
 $linkDOM.removeAttribute("rel"); // borramos el atributo
-console.log($linkDOM.hasAttribute("rel"));
 
 
-// Data-Attributes
+// Data-Attributes ( para crear nuestros propios atributos)
 
-console.log($linkDOM.getAttribute("data-description"));
 console.log($linkDOM.dataset); // me ba a guardar en un mapa todos los data-atributos que tenemos en el documento
 
-console.log($linkDOM.dataset.description);
 $linkDOM.setAttribute("data-description", "modelo de objeto del documento");
-
-console.log($linkDOM.dataset.description);
-
 $linkDOM.dataset.description = "otra forma de cambiar el valor de la propiedad";
+
+console.log($linkDOM.getAttribute("data-description"));
 console.log($linkDOM.dataset.description);
 
 console.clear();
 
-// clase 64: estilos y variables css
+
+//estilos y variables css
 
 console.log($linkDOM.style); // este se suele usar mas!
 console.log($linkDOM.getAttribute("style"));
 
 console.log($linkDOM.style.color);
-//console.log(this.getComputedStyle($linkDOM)); // me da todas las propiedades del enlace
-$linkDOM.style.setProperty("text-decoration", "none");
-//console.log(getComputedStyle($linkDOM).getPropertyValue("color")); // esta es otra forma de acceder a la propiedad color
-
 
 // formas de cambiar el valor a una propiedad
 $linkDOM.style.setProperty("text-decoration", "none");
-$linkDOM.style.display = "block";
-$linkDOM.style.width = "50%";
-$linkDOM.style.textAlign = "center";
-$linkDOM.style.marginLeft = "auto";
-$linkDOM.style.marginRigth = "auto";
-$linkDOM.style.padding = "1rem";
-$linkDOM.style.borderRadius = "5rem";
+$linkDOM.style.width = "25%";
+$linkDOM.style.padding = "12px";
+$linkDOM.style.borderRadius = "2rem";
 
 //variables en css
 
@@ -116,20 +76,20 @@ const $html = document.documentElement,
 let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
     varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
 
-console.log(varDarkColor, varYellowColor);
-
 $body.style.backgroundColor = varDarkColor;
+$body.style.setProperty("background-color", varDarkColor); // es otra forma de aserlo
+
 $body.style.color = varYellowColor;
 
-
 //$html.style.setProperty("--dark-color", "pink");// asi modifico las propiedades de la variable css
-varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
-//$body.style.backgroundColor = varDarkColor;
-$body.style.setProperty("background-color", varDarkColor); // es otra forma de aserlo
+
+$body.style.backgroundColor = "#222";
+$body.style.color = "white";
 
 console.clear();
 
-// clase 65: clases en css
+
+//clases en css
 
 const $card = document.querySelector(".card"); // me da el primero
 
@@ -144,24 +104,20 @@ $card.classList.replace("rotate-45", "rotate-135");
 
 $card.classList.add("opacity-80", "sepia"); // puedo agregar mas de una clase a la ves si la reparo por comas, lo mismo pasa con REMOVE y TOGGLE
 
+$card.classList.remove("rotate-135");
+$card.classList.remove("sepia");
+
 console.clear();
 
-// clase 66: manejo de texto
 
+//manejo de texto
 
 const $whatIsDOM = document.getElementById("que-es");
 
 let text = `
-    <p>
-    El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model </i></b>) es un                    
-API para documentos HTML y XML.
-    </p>
-    <p>
-    Éste provée una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
-    </p>
-    <p>
-        <mark> El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
-    </p>`
+    <h2>DOM: Document Object Model</h2>
+    <h2>BOM: Browser Object Model</h2>
+    <h2>CSSOM: CSS Object Model</h2>`
 ;
 
 $whatIsDOM.textContent = text; // no interpreta el codigo html y lo pone todo de corrido
@@ -169,11 +125,9 @@ $whatIsDOM.innerHTML = text; // interpreta el codigo html
   
 $whatIsDOM.outerHTML = text; // se usa para reemplazar la etiqueta que tenia antes por la que le paso, osea que ya no voy a tener el elemento "que es", ahora tendre tres parrafos por separado, sin ninguna caja que las contenga
 
-$body.style.backgroundColor = "white";
-$body.style.color = "#222";
-$card.classList.remove("rotate-135");
 
-// clase 67: recorriendo los elementos del DOM
+
+//recorriendo los elementos del DOM
 
 let $cards = document.querySelector(".cards");
 
@@ -187,96 +141,61 @@ console.log($cards.parentElement); // el elemento padre
 console.log($cards.previousElementSibling); // da el elemento que esta antes de que aparesca la clase
 console.log($cards.nextElementSibling);
 
-console.clear();
+//$cards.removeChild($cards.lastElementChild);  // elimina el ultimo elemento
 
-// clase 68: creando elementos dinamicamente
+console.clear();    
 
+
+// creando elementos dinamicamente
 
 const $figure = document.createElement("figure"),
     $img = document.createElement("img"),
-    $figcaption = document.createElement("figcaption"),
-    $figcaptionText = document.createTextNode("People");
-
-
-$img.setAttribute("src", "https://placeimg.com/200/200/people");
-$img.setAttribute("alt", "People");
+    $figcaption = document.createElement("figcaption");
 
 $figure.classList.add("card");
+$img.setAttribute("src", "https://placeimg.com/400/200/people");
+$figcaption.textContent = "People";
 
-$figcaption.appendChild($figcaptionText);
+$cards.appendChild($figure);  
 $figure.appendChild($img);
 $figure.appendChild($figcaption);
-$cards.appendChild($figure);  
 
+//
 
-const estaciones = ["verano", "invierno", "otoño", "primavera"],
-    $ul = document.createElement("ul");
+const contenido = ["seleccionar elementos", "atributos", "data-attributes", "estilos css", "variables css", "classList", "manejo de texto", "recorrer los elementos hijos", "crear elementos dinamicamente", "templates html", "insertando elementos"];
 
-document.write("<h3>Estaciones del año</h3>");
-document.body.appendChild($ul);
+const $ul = document.createElement("ul"),
+    $h2 = document.createElement("h2");
 
-estaciones.forEach((el) => {
+$h2.textContent = "Contenido";
+    
+contenido.forEach((el) => {
     const $li = document.createElement("li");
     $li.textContent = el;
     $ul.appendChild($li);
 })
+    
+const $divContenido = document.querySelector(".contenido").children[1];
 
+$divContenido.appendChild($h2);
+$divContenido.appendChild($ul);
 
 // en este caso estamos insertando los elementos uno por uno, en un ejemplo real donde hay que insertar 100 productos es ineficiente hacerlos uno a uno ya que el proceso cuesta, lo que se debe hacer en ese caso es cargarlo todo en un "fragmento" y despues hacer un sola insercion, esto mejora el rendimiento de la aplicacion
 
 
-const meses = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-  ];
+//templates html 
+// y usamos fragmento
 
-const $ul3 = document.createElement("ul"),
+const $template = document.getElementById("template-card").content,
     $fragment = document.createDocumentFragment();
-
-meses.forEach((el) => {
-  const $li = document.createElement("li");
-  $li.textContent = el;
-  $fragment.appendChild($li);
-});
-
-document.write("<h3>Meses del Año</h3>");
-$ul3.appendChild($fragment);
-document.body.appendChild($ul3);
-
-
-// clase 69: templates html
-
-const $template = document.getElementById("template-card").content;
 const cardContent = [
         {
-            title: "Tecnología",
-            img:"https://placeimg.com/200/200/tech"
+            title: "Animals",
+            img:"https://placeimg.com/400/200/animals"
         },
         {
-            title: "Animales",
-            img:"https://placeimg.com/200/200/animals"
-        },
-        {
-            title: "Arquitectura",
-            img:"https://placeimg.com/200/200/arch"
-        },
-        {
-            title: "Gente",
-            img:"https://placeimg.com/200/200/people"
-        },
-        {
-            title: "Naturaleza",
-            img:"https://placeimg.com/200/200/nature"
+            title: "Nature",
+            img:"https://placeimg.com/400/200/nature"
         }
     ];
 
@@ -293,34 +212,28 @@ cardContent.forEach(el => {
  $cards.appendChild($fragment);
 
 
- // clase 70: modificando elementos (old style)
+ // insertar elemento
 
  const $newCard = document.createElement("figure");
 
  $newCard.innerHTML =  `
-    <img src = "https://placeimg.com/200/200/any" alt="Any">
+    <img src = "https://placeimg.com/400/200/any" alt="Any">
     <figcaption>Any</figcaption>
  `;
 
  $newCard.classList.add("card");
 
- //$cards.replaceChild($newCard, $cards.children[2]); // para reemplazar un elemento
+ //$cards.insertBefore($newCard, $cards.firstChild);  // se agrega antes del primer hijo
+ $cards.insertAdjacentElement("afterbegin", $newCard); // se agrega como primer hijo
+ //$cards.insertAdjacentElement("beforeend", $newCard); // se agrega como ultimo hijo
 
-//$cards.insertBefore($newCard, $cards.firstChild); // insertara any antes de la primera
+ //$cards.replaceChild($newCard, $cards.children[2]); // para reemplazar un elemento por el nuevo
 
-//$cards.removeChild($cards.lastElementChild);  // elimina el ultimo elemento
-
-
-//nuevas formas de hacer lo mismo
-
-$cards.insertAdjacentElement("afterbegin", $newCard); // pone al nuevo elemento como primer hijo
-$cards.insertAdjacentElement("beforeend", $newCard); // se agrega como ultimo hijo
 
 /*
 .insertAdjacent... 
     .insertAdjacentElement(posicion, elemento);
     .insertAdjacentHTML(posicion, codigo html);
-    .insertAdjacentText(posicion, texto);
 
 posiciones:
     beforebegin = hermano anterior
@@ -332,18 +245,17 @@ posiciones:
 const $newCard2 = document.createElement("figure");
 
 let $contenCard =  `
-    <img src = "https://placeimg.com/200/200/any" alt="Any">
-    <figcaption> </figcaption>
+    <img src = "https://placeimg.com/400/200/arch" alt="Arch">
+    <figcaption>Arch</figcaption>
  `;
 
 $newCard2.classList.add("card");
 
 $newCard2.insertAdjacentHTML("beforeend", $contenCard);
-$newCard2.querySelector("figcaption").insertAdjacentText("afterbegin", "hola");
 $cards.insertAdjacentElement("afterbegin", $newCard2);
 
-$cards.prepend($newCard2); // lo agrega como primer hijo
-$cards.append($newCard2); // lo agrega como ultimo hijo
 $cards.before($newCard2); // lo agrega antes de cards (osea como hermano anterior)
 $cards.after($newCard2); // lo agrega como hermano posterior
+$cards.append($newCard2); // lo agrega como ultimo hijo
+$cards.prepend($newCard2); // lo agrega como primer hijo
 

c5a9fd733d7fc860d90548ce2ae87d860dc6cdf6
alejandroBarua <2baruaalejandro95@gmail.com>
2021-04-23 21:04:47 -0300

    simplifique lineas de codigo usando un array

--- AJAX/js/funciones.js
+++ AJAX/js/funciones.js
@@ -4,67 +4,24 @@ export default function lastDate(lastDate) {
         resultado = dateNow - toMilliseconds(lastDate),
         days = Math.floor(resultado / (1000*60*60*24));
 
+    const numbers = ["", "", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once", "doce"];
+
     if(days == 0) return "hoy";
 
     if(days == 1) return "hace un dia"
-    if(days < 7) return numberToWord(days) + " dias";
+    if(days < 7) return `hace ${numbers[days]} dias`;
 
     let weeks = Math.floor(days / 7);
     if(weeks == 1) return "hace una semana";
-    if(weeks < 4)  return numberToWord(weeks) + " semanas";
+    if(weeks < 4)  return `hace ${numbers[weeks]} semanas`;
     
     let months = Math.floor(weeks / 4);
     if(months == 1) return "hace un mes";
-    if(months <= 12) return numberToWord(months) + " meses";
+    if(months <= 12) return `hace ${numbers[months]} meses`;
 
     let years = Math.floor(months / 12);
     if(years == 1) return "hace un año";
-    return numberToWord(years) + " años";
-}
-
-
-function numberToWord(number) {
-    
-    let word = "hace ";
-
-    switch(number){
-
-        case 2:
-            word += "dos";
-            break;
-        case 3:
-            word += "tres";
-            break;
-        case 4:
-            word += "cuatro";
-            break;
-        case 5:
-            word += "cinco";
-            break;
-        case 6:
-            word += "seis";
-            break;
-        case 7:
-            word += "siete";
-            break;
-        case 8:
-            word += "ocho";
-            break;
-        case 9:
-            word += "nueve";
-            break;
-        case 10:
-            word += "diez";
-            break;
-        case 11:
-            word += "once";
-            break;
-        case 12:
-            word += "doce";
-            break;
-    }
-
-    return word;
+    return `hace ${numbers[years]} años`;
 }
 
 function toMilliseconds(updatedAt) {
